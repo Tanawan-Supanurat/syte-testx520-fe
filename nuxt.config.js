5e8889d5d886316e7b3f4af73d508559959bdd97
Tanawan-Supanurat <tanawan.supanurat@jp.fujitec.com>
2022-10-05 16:06:24 +0900

    サブディレクトリ作成

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,6 +21,13 @@ export default {
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    base:{
+      href: 'router.base',
+    },
+  },
+
+  router: {
+    base: '/syte-testx520-fe',
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
